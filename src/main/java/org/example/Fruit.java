56ef1368ad62a9272812954fce6dd5c73a6cb7d6
avinashnarayan439 <152829997+avinashnarayan439@users.noreply.github.com>
2023-12-09 09:50:38 +0530

    Update Fruit.java

--- src/main/java/org/example/Fruit.java
+++ src/main/java/org/example/Fruit.java
@@ -3,9 +3,11 @@ package org.example;
 public class Fruit {
     String colour;
     String taste;
-    public Fruit(String  colour, String taste)
+    String name;
+    public Fruit(String  colour, String taste,String name)
         {this.colour=colour;
         this.taste=taste;
+         this.name=name;
         }
         public boolean isTasty(){
         return taste.equals("sweet");
