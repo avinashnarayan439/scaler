72a9ca10c5f191ef2a49cd0c243e200a253eb1f7
avinashnarayn439 <avinash.narayan439>
2023-12-09 09:13:50 +0530

    its my scaler gradle project

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("its my first gradle project");
+
+    }
+
+}
